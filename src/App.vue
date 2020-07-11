993bbb65ec59ebfbaa7551c995f696e89503ebaf
Anton Sapega <antonsapega@gmail.com>
2020-07-11 03:25:37 +0300

    Task about three buttons is complete

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <Button class="btn" :btnName="randomNumber" @callAlert="callAlertHandler" />
-    <Button class="btn" :btnName="randomLetter" @callAlert="callAlertHandler" />
-    <Button class="btn" :btnName="quantityClicks" @callAlert="callAlertHandler" />
+    <Button class="btn" :btnName="randomNumber" @callAlert="generateRandomNumber" />
+    <Button class="btn" :btnName="randomLetter" @callAlert="generateRandomLetter" />
+    <Button class="btn" :btnName="quantityClicks" @callAlert="valueOfClicks" />
   </div>
 </template>
 
@@ -19,13 +19,25 @@ export default {
     return {
       randomNumber: 'Random number',
       randomLetter: 'Random letter',
-      quantityClicks: 'Quantity of clicks'
+      quantityClicks: 'Quantity of clicks',
+
+      clicks: 0
     }
   },
 
   methods: {
-    callAlertHandler() {
-      alert('Hello from parent')
+    generateRandomNumber() {
+      alert(Math.floor(Math.random() * 1000))
+    },
+
+    generateRandomLetter() {
+      const randomLetterPosition = Math.round( 65 - 0.5 + Math.random() * (90 - 65 + 1) )
+      alert(String.fromCodePoint( Math.floor(randomLetterPosition) ))
+    },
+
+    valueOfClicks() {
+      this.clicks++
+      alert(this.clicks)
     }
   }
 }
